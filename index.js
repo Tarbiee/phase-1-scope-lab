35d0aab7be3972eb0e5e79a61dd57a7fbc18ec16
Tarbiee <dorcusstaby@gmail.com>
2023-09-22 13:57:13 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+const leastFavoriteCustomer = "luvuno";
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){ 
+    bestCustomer = 'not bob';   // declaring a variable without a keyword makes it globally scoped
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "tabby";
+}
